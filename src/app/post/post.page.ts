bb28ad47765ec10b52386d3cb492ca0c3a8fd6ec
Quillinan <milton.quillinan1798@gmail.com>
2020-01-30 22:21:27 -0300

    TrabalhoTSfull

--- src/app/post/post.page.ts
+++ src/app/post/post.page.ts
@@ -10,54 +10,60 @@ export class PostPage implements OnInit {
   replys;
 
   constructor() {
-    this.replys = [
+    this.replys = [  // Lista reply que será chamada no for com valores pré-definidos que são alterados com as demais funções
       {reply_nome : 'Joao',
       reply_texto : 'Muito ruim a morte do protagonista',
       reply_data : '3 semanas',
       numero_like: 0,
-      numero_deslike: 4,
+      numero_dislike: 4,
       like_state: false,
-      deslike_state: true,
+      dislike_state: true,
+      spoiler_state: null,
       },
       {reply_nome : 'Maria',
       reply_texto : 'Se te vejo na rua te mato',
       reply_data : '2 semanas',
       numero_like: 4,
-      numero_deslike: 0,
+      numero_dislike: 0,
       like_state: true,
-      deslike_state: false,
+      dislike_state: false,
+      spoiler_state: null,
       },
       {reply_nome : 'Adalberto',
       reply_texto : 'Reza pra genta nunca esbarrar',
       reply_data : '2 semanas',
       numero_like: 4,
-      numero_deslike: 0,
+      numero_dislike: 0,
       like_state: true,
-      deslike_state: false,
+      dislike_state: false,
+      spoiler_state: null,
       },
       {reply_nome : 'Creusa',
       reply_texto : 'Cade os adm dessa joça??',
       reply_data : '1 semana',
       numero_like: 4,
-      numero_deslike: 0,
+      numero_dislike: 0,
       like_state: false,
-      deslike_state: false,
+      dislike_state: false,
+      spoiler_state: null,
       },
       {reply_nome : 'Lurdes',
       reply_texto : 'BANE ELE!!!!',
       reply_data : '6 dias',
       numero_like: 4,
-      numero_deslike: 0,
+      numero_dislike: 0,
       like_state: true,
-      deslike_state: false,
+      dislike_state: false,
+      spoiler_state: null,
       },
       {reply_nome : 'Alvaro',
-      reply_texto : 'Ufa o sistema de spoiler me salvou',
+      reply_texto : 'Ufa o sistema de spoilers me salvou',
       reply_data : '5 dias',
       numero_like: 2,
-      numero_deslike: 0,
+      numero_dislike: 0,
       like_state: true,
-      deslike_state: false,
+      dislike_state: false,
+      spoiler_state: null,
       }
     ]
    }
@@ -70,14 +76,13 @@ export class PostPage implements OnInit {
     reply_data:string;
   	compartilhamento:number;
     numero_like:number;
-    numero_deslike:number;
+    numero_dislike:number;
     like_state:boolean;
-    deslike_state:boolean;
-    spoiler_state:boolean = false;
+    dislike_state:boolean;
+    spoiler_state:number = null;
 
 
-
-    public like(reply: any):void{
+    public like(reply: any):void{ // Função que define as cores a partir do like_state e balanceia os números de likes.
       if (reply.like_state){
         reply.numero_like--;
         reply.like_state = false;
@@ -85,22 +90,22 @@ export class PostPage implements OnInit {
       else{
         reply.numero_like++;
         reply.like_state = true;
-        if(reply.deslike_state){
-          reply.deslike_state = false;
-          reply.numero_deslike--;
+        if(reply.dislike_state){
+          reply.dislike_state = false;
+          reply.numero_dislike--;
         }
       }
 
       }
 
-      public deslike(reply: any):void{
-        if (reply.deslike_state){
-          reply.numero_deslike--;
-          reply.deslike_state = false;
+      public dislike(reply: any):void{ // Função que define as cores a partir do dislike_state e balanceia os números de dislikes.
+        if (reply.dislike_state){
+          reply.numero_dislike--;
+          reply.dislike_state = false;
         }
         else{
-          reply.numero_deslike++;
-          reply.deslike_state = true;
+          reply.numero_dislike++;
+          reply.dislike_state = true;
           if(reply.like_state){
             reply.like_state = false;
             reply.numero_like--;
@@ -109,8 +114,13 @@ export class PostPage implements OnInit {
 
         }
 
-        public spoiler(reply: any):void{
-          this.spoiler_state = true;
+        public spoiler(reply: any):void{ // Função que define altera o spoiler_state para ativar o aviso.
+          if(reply.spoiler_state == null){
+            reply.spoiler_state = 1;
+          }
+          else{
+            reply.spoiler_state = null;
+          }
 
         }
 
